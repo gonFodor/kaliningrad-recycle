2782fdd2208258f7dfbdf44f3a9edf5035e0aed8
gonFodor <ferosha1997@gmail.com>
2025-08-14 22:46:33 +0200

    d

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/kaliningrad-recycle/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
